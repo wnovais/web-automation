533613cbaac623371e42783d369666ee53a59a61
Willian Novais <willian.novais@hotmail.com>
2023-03-06 17:58:33 -0300

    first commit

---
+++ WebAutomatioApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Abre nova janela do Chrome, navega até o site do Google, localiza a barra de pesquisa e faz a busca com o tema "hello, world"
+        var options = new ChromeOptions();
+        options.AddArgument("--start-maximized");
+        var driver = new ChromeDriver(options);
+        driver.Navigate().GoToUrl("https://www.google.com");
+        var input = driver.FindElement(By.Name("q"));
+        input.SendKeys("hello, world");
+        input.Submit();
+        Console.Read();
+    }
+}
